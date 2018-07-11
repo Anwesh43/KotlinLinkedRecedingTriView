c3b75670659bbab33ace10b7f853c3fc2e51e264
Anwesh43 <anweshthecool0@gmail.com>
2018-07-11 11:37:14 +0530

    importing libraries and adding number of nodes

--- linkedtrirecview/src/main/java/com/anwesh/uiprojects/linkedtrirecview/LinkedRecTriView.kt
+++ linkedtrirecview/src/main/java/com/anwesh/uiprojects/linkedtrirecview/LinkedRecTriView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.linkedtrirecview
 /**
  * Created by anweshmishra on 11/07/18.
  */
+
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Path
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+
+val NODES : Int = 5
