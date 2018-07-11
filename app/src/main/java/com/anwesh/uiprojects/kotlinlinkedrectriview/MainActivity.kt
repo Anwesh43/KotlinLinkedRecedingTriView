65a0cc39b3982c8e64974e99e5cfa1562b9b189e
Anwesh43 <anweshthecool0@gmail.com>
2018-07-11 12:09:58 +0530

    adding static create method

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedrectriview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedrectriview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedrectriview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedtrirecview.LinkedRecTriView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinkedRecTriView.create(this)
     }
 }
